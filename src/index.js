c2c3520b2bb0ea5b4fc6f7e84b16b334ac3bc946
Cody Brewer <brewer.cody.m@gmail.com>
2019-07-30 19:38:13 -0600

    initial commit,  some setup

--- src/index.js
+++ src/index.js
@@ -4,6 +4,20 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const firebase = require('firebase');
+require('firebase/firestore');
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBZQXXApj9YpxK2UuRIo4Ybofqf2nA7nkY',
+  authDomain: 'evernote-clone-tutorial-6de48.firebaseapp.com',
+  databaseURL: 'https://evernote-clone-tutorial-6de48.firebaseio.com',
+  projectId: 'evernote-clone-tutorial-6de48',
+  storageBucket: '',
+  messagingSenderId: '580131787800',
+  appId: '1:580131787800:web:47b480d941cf91e6'
+};
+firebase.initializeApp(firebaseConfig);
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
